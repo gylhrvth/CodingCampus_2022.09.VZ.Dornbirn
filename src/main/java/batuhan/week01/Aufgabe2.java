19d13a19de454c4dd1f84b622df0e7c41b5d2c46
batuhan <batuhandanisment@hotmail.com>
2022-09-12 15:37:33 +0200

    aufgabe 2

--- src/main/java/batuhan/week01/Aufgabe2.java
+++ src/main/java/batuhan/week01/Aufgabe2.java
@@ -2,11 +2,13 @@ package batuhan.week01;
 
 public class Aufgabe2 {
     public static void main(String[] args) {
-        for (int i=0; i<10; ++i){
-            if (i=0) {
-                System.out.print(0);
+        for (int i = 0; i < 10; ++i){
+            if (i > 0) {
+                System.out.print(", ");
             }
+            System.out.print(i);
         }
+        System.out.println();
     }
 
 }
