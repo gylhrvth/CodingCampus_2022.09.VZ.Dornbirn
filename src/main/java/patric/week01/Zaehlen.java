9ad131dcf2181162a19f35242627219cb51c71f9
PatricBoso <mr.patric.boso@gmail.com>
2022-09-12 15:14:09 +0200

    Gerade Zahlen

--- src/main/java/patric/week01/Zaehlen.java
+++ src/main/java/patric/week01/Zaehlen.java
@@ -2,7 +2,24 @@ package patric.week01;
 
 public class Zaehlen {
     public static void main(String[] args) {
-        for(int i = 1; i <= 10; ++i){
+        for (int i = 0; i <= 10; ++i) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
             System.out.print(i);
+        }
+        System.out.println();
+        System.out.println("============================");
+
+        int j = 0;
+        while (j <= 10) {
+            System.out.println(j);
+
+            ++j;
+
+        }
+        System.out.println("============================");
 
 
+    }
+}
