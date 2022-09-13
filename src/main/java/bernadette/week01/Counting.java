a67249765d3db2ddb5d417b6374476ab0d048a1f
Berna Koechle <k.berna@gmx.at>
2022-09-13 09:19:36 +0200

    basic counting and output tasks

--- src/main/java/bernadette/week01/Counting.java
+++ src/main/java/bernadette/week01/Counting.java
@@ -8,25 +8,27 @@ public class Counting {
 
 
         for (int i = 0; i <= 10; ++i) {
-            System.out.print(i +",");
+            System.out.print(i + ",");
         }
+
         System.out.println("");
         System.out.println("===== mit while statt for =======");
+
         int j = 0;
         while (j <= 10) {
-            System.out.print(j+",");
+            System.out.print(j + ",");
             ++j;
-
-
         }
+
         System.out.println("");
         System.out.println("");
         System.out.println("");
         System.out.println("===========================");
         System.out.println("2.Aufgabe");
         System.out.println("===========================");
+
         for (int i = 0; i <= 9; ++i) {
-            System.out.print("i = "+i + ",");
+            System.out.print("i = " +i + ",");
         }
 
         System.out.println("");
@@ -39,12 +41,11 @@ public class Counting {
         for (int i = 1; i <= 10; ++i) {
 
             if ( i % 2 == 0) {
-                System.out.print(i +",");
+                System.out.print(i + ",");
             }
-
-
-
         }
+
+
         System.out.println("");
         System.out.println("");
         System.out.println("");
@@ -56,16 +57,29 @@ public class Counting {
         while (k <= 10) {
 
             if ( k % 2 == 0) {
-                System.out.println(k+ " was the variable k");
+                System.out.println(k + " was the variable k");
                 }
-
             ++k;
         }
+
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("===========================");
+        System.out.println("3.Aufgabe, (b), easier way");
+        System.out.println("===========================");
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.print(i + ",");
+        }
+
+        System.out.println("");
+        System.out.println("");
         System.out.println("===========================");
         System.out.println("4.Aufgabe");
         System.out.println("===========================");
         for (int i = 10; i >= -10; i = i-1) {
-            System.out.print(i +",");
+            System.out.print(i + ",");
         }
 
         System.out.println("");
