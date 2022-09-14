22ce0cdcab27dfa9191525a462f251901fbc57cf
Alp <alpsahin60@outlook.com>
2022-09-14 15:43:18 +0200

    Printcharacters

--- src/main/java/alp/week01/PrintCharacters.java
+++ src/main/java/alp/week01/PrintCharacters.java
@@ -9,30 +9,54 @@ public class PrintCharacters {
         System.out.println();
         printRect("o ", 10, 3);
         System.out.println();
-
+        printTriangleBottomLeft("x", 4);
+        System.out.println();
+        printTriangleTopLeft("d", 5);
     }
 
-    public static void printchars(String x, int size) {
+    public static void printchars(String text, int size) {
         for (int i = 0; i < size; ++i) {
-            System.out.print(x);
+            System.out.print(text);
         }
         System.out.println();
     }
 
-    public static void printsquare(String x, int length) {
-        for (int rows = 0; rows < length; rows++) {
-            for (int i = 0; i < length; i++) {
-                System.out.print(x);
+    public static void printsquare(String text, int size) {
+        for (int rows = 0; rows < size; rows++) {
+            for (int i = 0; i < size; i++) {
+                System.out.print(text);
             }
             System.out.println();
         }
 
     }
 
-    public static void printRect(String x, int width, int height) {
+    public static void printRect(String text, int width, int height) {
         for (int rows = 0; rows < height; rows++) {
             for (int i = 0; i < width; i++) {
-                System.out.print(x);
+                System.out.print(text);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printTriangleBottomLeft(String text, int size) {
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(text);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void printTriangleTopLeft(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i <= j)
+
+                    System.out.print(text);
             }
             System.out.println();
         }
@@ -40,6 +64,7 @@ public class PrintCharacters {
 
 
 }
+
 
 
 
