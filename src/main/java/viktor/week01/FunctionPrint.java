7d6a19cde343e5a9a35a34cdcddd2872421bb303
lukas <lukas.aichbauer@gmail.com>
2022-09-15 11:25:29 +0200

    - Rehearsal.

--- src/main/java/viktor/week01/FunctionPrint.java
+++ src/main/java/viktor/week01/FunctionPrint.java
@@ -2,18 +2,14 @@ package viktor.week01;
 
 public class FunctionPrint {
 
-    public static void printChars(String f, int n){
-        for(int i=1; i <= n; i++)
-        {
+    public static void printChars(String f, int n) {
+        for (int i = 1; i <= n; i++) {
             System.out.print(f);
         }
     }
 
     public static void main(String[] args) {
-        printChars("x",10);
+        printChars("x", 10);
         System.out.println();
-
-
-
     }
 }
