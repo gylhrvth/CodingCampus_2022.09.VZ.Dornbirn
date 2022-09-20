53231171051115d85672fc66f4cc2f254125f5fd
Alp <alpsahin60@outlook.com>
2022-09-20 09:36:46 +0200

    Printcharacters

--- src/main/java/alp/week01/PrintCharacters.java
+++ src/main/java/alp/week01/PrintCharacters.java
@@ -1,5 +1,7 @@
 package alp.week01;
 
+import javax.imageio.plugins.tiff.TIFFImageReadParam;
+
 public class PrintCharacters {
 
     public static void main(String[] args) {
@@ -12,6 +14,26 @@ public class PrintCharacters {
         printTriangleBottomLeft("x", 4);
         System.out.println();
         printTriangleTopLeft("d", 5);
+        System.out.println();
+        printTriangleTopRight("d", 5);
+        System.out.println();
+        printTriangleBottomRight("x", 5);
+        System.out.println();
+        printEmptySquare("x", 10);
+        System.out.println();
+        printEmptySquare2("A", 3);
+        System.out.println();
+        printSlash("x", 3, true);
+        System.out.println();
+        printSlash("x", 4, false);
+        System.out.println();
+        printTriangle("x", 7);
+        System.out.println();
+        PrintRhombus("x", 7);
+        System.out.println();
+        printX("q",5);
+        System.out.println();
+
     }
 
     public static void printchars(String text, int size) {
@@ -62,8 +84,162 @@ public class PrintCharacters {
         }
     }
 
+    public static void printTriangleTopRight(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
 
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(text);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void printTriangleBottomRight(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(text);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void printEmptySquare(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(text);
+        }
+        System.out.println();
+
+        for (int j = 0; j < size - 2; j++) {
+            System.out.print(text);
+            for (int i = 0; i < size - 2; i++) {
+                System.out.print(" ");
+            }
+            System.out.println(text);
+        }
+        for (int j = 0; j < size; j++) {
+            System.out.print(text);
+        }
+        System.out.println();
+    }
+
+    public static void printEmptySquare2(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(text);
+        }
+        System.out.println();
+
+        for (int i = 0; i < size - 2; i++) {
+            System.out.print(text);
+            for (int j = 0; j < size - 2; j++) {
+                System.out.print(" ");
+            }
+            System.out.print(text);
+            System.out.println();
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(text);
+        }
+        System.out.println();
+    }
+
+    public static void printSlash(String text, int size, boolean a) {
+        if (a) {
+            printSlashA(text, size);
+        } else {
+            printSlashB(text, size);
+        }
+    }
+
+    public static void printSlashA(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println(text);
+        }
+    }
+
+    public static void printSlashB(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println(text);
+        }
+    }
+
+    public static void printTriangle(String text, int size) {
+        for (int i = 0; i < size; i++) {  //länge nach unten
+            for (int j = 0; j < 2 * size - 1; j++) { //breite
+                if (i == size - 1) { // Letzte Zeile
+                    System.out.print(text);
+                } else if (i + j == size - 1) { // Linke Seite
+                    System.out.print(text);
+                } else if (i == j - size + 1) { // Rechte Seite
+                    System.out.print(text);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void PrintRhombus(String text, int size) {
+        for (int i = 0; i < 2 * size -1; i++) {  //länge nach unten. (länge und breite ist dasselbe bei rhombus, deshalb (2*size-1 gleich)).
+            for (int j = 0; j < 2 * size - 1; j++) { //breite nach rechts. (breite und länge ist dasselbe bei rhombus, deshalb (2*size-1 gleich)).
+                if (i + j == size - 1) { // Linke Seite - schräge von unten links zur mitte hin nach oben.
+                    System.out.print(text);
+                } else if (i == j - size + 1) { // Rechte Seite - schräge von der mitte oben nach unten rechts.
+                    System.out.print("a");
+                } else if (i + j == 3*(size - 1)) { // Rechte Seite negativ - von unten rechts nach unten zur mitte.
+                        System.out.print("q");
+                } else if (i - j == size -1 ) { // Linke Seite negativ - von der mitte nach oben links.
+                    System.out.print("@");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+
+
+        }
+    }
+
+    public static void printX(String text, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size*2+1; j++) {
+                System.out.println(text);
+
+            }
+
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/main/java/alp/week02/PrimitiveDatenTypen.java
@@ -0,0 +1,95 @@
+package alp.week02;
+
+public class PrimitiveDatenTypen {
+    public static void main(String[] args) {
+        printFaktorial(20);
+        System.out.println();
+        printFaktorialErweitert(20);
+        System.out.println();
+        zaehlenDezimal(100);
+        System.out.println();
+        zaehlenDezimalErweitert(100);
+        System.out.println();
+        teilenHerrschen(20);
+        System.out.println();
+        teilenHerrschen2(20);
+        System.out.println();
+        leibnizReihe(1000000);
+        System.out.println();
+
+    }
+
+    public static void printFaktorial(int count) {
+        long result = 1;
+        for (int i = 1; i <= count; i++) {
+            result = result * i;
+            System.out.println(i + "! = " + result);
+            {
+
+            }
+        }
+
+
+    }
+
+    public static void printFaktorialErweitert(int count) {
+        long result = 1;
+        for (int i = 1; i <= 20; i++) {
+            result = result * i;
+            System.out.printf("%2d! = %19d%n", i, result); // %2d(rechtsbündig) steht für die zahl links vom = // %19d sorgt dafür dass 19zahlen platz haben rechts vom =
+           // System.out.println();                        // %-2d(linksbündig) d-steht für zahl, s-steht für string(text)
+                                                            // %n steht für zeilenumbruch. entweder mit println oder mit %n kann man zeilenumbruch machen
+        }
+
+
+    }
+
+    public static void zaehlenDezimal(int count) {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i / 10f);
+        }
+        for (float i = 0; i <= 100; i += 0.1f) {
+            System.out.println(i);
+
+        }
+
+    }
+
+    public static void zaehlenDezimalErweitert(int count) {
+        for (float i = 0; i <= count; i += 0.1f) {
+            System.out.printf("%5.1f", i);
+            System.out.println();
+
+        }
+    }
+
+    public static void teilenHerrschen(int count) {
+        for (int i = 0; i <= count; i++) {
+            System.out.println(i / 5);
+
+
+        }
+    }
+
+    public static void teilenHerrschen2(float count) {
+        for (float i = 0; i <= count; i++) {
+            System.out.println(i / 5);
+        }
+
+    }
+
+    public static void leibnizReihe(int count) {
+        double pi=0;
+        boolean jump = true;
+        for (int i = 1; i <= count; i= i+2) {
+            if (jump){
+                pi=pi + 4f / i;
+                jump=false;
+            } else {
+                pi=pi - 4f / i;
+                jump=true;
+            }
+        }
+        System.out.println(pi);
+    }
+}
