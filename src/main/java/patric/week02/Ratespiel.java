a64f6b7e571999b56dc4a7c5be58e230fe3361a6
PatricBoso <mr.patric.boso@gmail.com>
2022-09-21 14:05:38 +0200

    210922

---
+++ src/main/java/patric/week02/Ratespiel.java
@@ -0,0 +1,39 @@
+package patric.week02;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Ratespiel {
+
+    static void rateZahl(int wertebereich){
+        int zufallsZahl = new Random().nextInt(wertebereich + 1);
+        //System.out.println(zufallsZahl);//Zur Überprüfung der Zufallszahl
+        Scanner sc = new Scanner(System.in);
+        int eingabe =0;//Eingabe startet jetzt bei Null
+        int zaehler = 0;// Startet bei Null
+        do {
+            try{
+                System.out.println("Gib eine Zahl zwischen 0 und " + wertebereich + " ein!");
+                eingabe = sc.nextInt();
+                zaehler++;
+                if (zufallsZahl < eingabe) {
+                    System.out.println("Du liegst falsch! Die zu erratende Zahl ist kleiner!");
+                } if(zufallsZahl < eingabe) {
+                    System.out.println("Du liegst falsch! Die zu erratende Zahl ist GRÖSSER!");
+                }sc.nextLine();
+            }catch (InputMismatchException ime) {
+                System.out.println("Es ist keine Zahl");
+            }
+        }
+        while (zufallsZahl != eingabe);//Schleifenbedingung im Fuß
+        System.out.println("Super! Du hast " + zaehler + " Durchgänge benötigt.");
+        return;
+
+    }
+    public static void main(String[] args) {
+        rateZahl(100);
+    }
+}
