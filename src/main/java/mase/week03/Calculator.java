ab11e93a6fe2ac39ecb4e7af75b1baa686ed3da6
mase52 <cajic.ma@gmail.com>
2022-09-26 15:15:53 +0200

    User Input mit Menü

--- src/main/java/mase/week03/Calculator.java
+++ src/main/java/mase/week03/Calculator.java
@@ -1,5 +1,6 @@
 package mase.week03;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
@@ -14,34 +15,40 @@ public class Calculator {
         String operator;
         boolean repeat = true;
 
-        while (repeat) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Wähle einen Operator +-*/");
+//        operator = scanner.nextLine();
+//
+//        System.out.println("Wähle eine Nummer");
+//        number1 = scanner.nextFloat();
+//
+//        System.out.println("Wähle eine weitere Nummer");
+//        number2 = scanner.nextFloat();
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Wähle einen Operator +-*/");
+                operator = scanner.nextLine();
+
+                System.out.println("Wähle eine Nummer");
+                number1 = scanner.nextFloat();
+
+                System.out.println("Wähle eine weitere Nummer");
+                number2 = scanner.nextFloat();
 
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Wähle einen Operator +-*/");
-            operator = scanner.nextLine();
+                switch (operator) {
 
-            System.out.println("Wähle eine Nummer");
-            number1 = scanner.nextFloat();
+                    case "+":
+                        result = number1 + number2;
+                        System.out.println("Das Ergebnis ist :" + result);
+                        break;
 
-            System.out.println("Wähle eine weitere Nummer");
-            number2 = scanner.nextFloat();
+                    default:
+                        System.out.println("Geben Sie nur einer  dieser Operatoren (+,*,-,/ ein");
+                        break;
 
 
-            switch (operator) {
-
-                case "+":
-                    result = number1 + number2;
-                    System.out.println("Das Ergebnis ist :" + result);
-                    break;
-
-                default:
-                    System.out.println("Geben Sie nur einer  dieser Operatoren (+,*,-,/ ein");
-                    break;
-
-            }
-            return result;
-        } repeat = false;
+                }return result;
     }
 }
 
