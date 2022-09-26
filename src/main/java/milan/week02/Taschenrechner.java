7dc1fb30ebcefcb73db212fa45a1b4189a5291d8
mailandr <splintersfinest@gmail.com>
2022-09-26 11:30:04 +0200

    Aufgabe Taschenrechner abgeschlossen.

--- src/main/java/milan/week02/Taschenrechner.java
+++ src/main/java/milan/week02/Taschenrechner.java
@@ -6,21 +6,57 @@ public class Taschenrechner {
 
     public static void main(String[] args) {
 
-        readFloatInput("Bitte gebe eine Kommazahl ein:");
-        //readOperatorInput();
+        float eingabe = readFloatInput("Bitte gebe eine Kommazahl ein:");
+        int auswahl = readOperatorInput("Bitte Operator angeben:\n1: +\n2: -\n3: *\n4: /", 1, 4);
+        float eingabe2 = readFloatInput("Bitte gebe eine zweite Kommazahl ein:");
+
+        if (auswahl == 1) {
+            System.out.println("Ergebnis: " + (eingabe + eingabe2));
+        } else if (auswahl == 2) {
+            System.out.println("Ergebnis: " + (eingabe - eingabe2));
+        } else if (auswahl == 3) {
+            System.out.println("Ergenis: " + (eingabe * eingabe2));
+        } else {
+            System.out.println("Ergebnis: " + (eingabe / eingabe2));
+        }
+
+//        String text = "Hello World";
+//        if (text.equals("Apfel")){
+//            System.out.println("Passt. :-)");
+//        }
     }
 
     public static float readFloatInput(String prompt) {
         Scanner sc = new Scanner(System.in);
         System.out.println(prompt);
         while (true) {
-            if (sc.hasNextFloat()){
+            if (sc.hasNextFloat()) {
                 return sc.nextFloat();
             } else {
                 System.out.println(prompt);
                 sc.nextLine();
             }
-
         }
     }
+
+
+    public static int readOperatorInput(String prompt, int minValue, int maxValue) {
+        Scanner opsc = new Scanner(System.in);
+        System.out.println(prompt);
+
+        while (true) {
+            if (opsc.hasNextInt()) {
+                int operator = opsc.nextInt();
+                if (operator < minValue || operator > maxValue) {
+                    System.out.println("Bitte eine Zahl zwischen: " + minValue + " und " + maxValue + " angeben!");
+                } else {
+                    return operator;
+                }
+            } else {
+                System.out.println("Bitte eine Zahl zwischen: " + minValue + " und " + maxValue + " angeben!");
+                opsc.nextLine();
+            }
+        }
+    }
+
 }
