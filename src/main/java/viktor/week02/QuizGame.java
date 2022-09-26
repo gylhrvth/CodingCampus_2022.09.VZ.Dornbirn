c56e3e2be78add490ab041088d38b9ff27d5d241
Viktor <selm.viktor@gmail.com>
2022-09-26 17:57:00 +0200

    QuizGame is fixed!

---
+++ src/main/java/viktor/week02/QuizGame.java
@@ -0,0 +1,39 @@
+package viktor.week02;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class QuizGame {
+
+    public static int readNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte eine Zahl angeben!");
+        return sc.nextInt();
+    }
+
+    public static void main(String[] args) {
+
+        Random randomNumber = new Random();
+        int random = randomNumber.nextInt(101);
+        System.out.println("Random:" + random);
+        int myNumber = 1;
+
+        while(myNumber != random) {
+
+            try{
+                myNumber = readNumber();
+                if (myNumber > random) {
+                    System.out.println("Die Zahl ist zu hoch!");
+
+                } else if (myNumber == random) {
+                    System.out.println("Getroffen, du hast gewonnen!");
+                } else {
+                    System.out.println("Die Zahl ist zu niedrig");
+                }
+            }catch(InputMismatchException ime){
+                System.out.println("Hier wurde keine Zahl angeben! Bitte eine Zahl angeben!");
+            }
+        }
+    }
+}
