6d77f2d7188ed61f6017604e7071f784d0216cb9
Alp <alpsahin60@outlook.com>
2022-09-26 10:02:33 +0200

    Zeile 66-70 eingefügt, else (bezieht sich auf die falsche eingabe)

--- src/main/java/alp/week02/AufgabeMenu.java
+++ src/main/java/alp/week02/AufgabeMenu.java
@@ -7,36 +7,46 @@ import java.util.Scanner;
 public class AufgabeMenu {
 
     public static void main(String[] args) {
+
         while (true) {
             printMenu();
             int choosenNumber = readNumber("Bitte wählen Sie nun?", 1, 3);
+
             System.out.println("Geben Sie das Zeichen ein.");
             String zeichen = readZeichen();
             System.out.println("Geben Sie eine Zahl von 1 bis 30");
+
             if (choosenNumber == 1) {
-                PrintCharacters.printSlashA(zeichen, readNumber("Wie groß", 1, 30));
+                PrintCharacters.printSlashA(zeichen, readNumber("Wie groß ?", 1, 30));
+
             } else if (choosenNumber == 2) {
-                PrintCharacters.printEmptySquare(zeichen, readNumber("Wie groß", 1, 30));
+                PrintCharacters.printEmptySquare(zeichen, readNumber("Wie groß ?", 1, 30));
+
                 //zeichne was anderes
             } else if (choosenNumber == 3) {
-                PrintCharacters.PrintRhombus(zeichen, readNumber("Wie groß", 1, 30));
+                PrintCharacters.PrintRhombus(zeichen, readNumber("Wie groß ?", 1, 30));
                 //zeichne was anderes
+
             }
+
+
             System.out.println("Möchten Sie noch etwas zeichnen?");
             String frage = readJaNein();
 
-          //  System.out.println("");
+            //  System.out.println("");
 
             if (!frage.equals("ja")) {
                 System.out.println("Auf Wiedersehen!");
                 break;
             }
         }
+
     }
+
 
     public static void printMenu() {
         System.out.println("Was möchten Sie zeichnen ?");
-        System.out.println("1) Christbaum");
+        System.out.println("1) PrintSlashA");
         System.out.println("2) Quader");
         System.out.println("3) Rhombus");
 
@@ -48,12 +58,16 @@ public class AufgabeMenu {
         while (true) {
             System.out.println(prompt);
             if (sc.hasNextInt()) {
-                int number = sc.nextInt();
+               int number = sc.nextInt();
                 //Frage ob Zahl gut ist?
                 if (number >= minValue && number <= maxValue) {
                     return number;
                 }
+            }else {
+                System.out.println("Falsche Eingabe");
+                System.out.println("Bitte Wählen Sie erneut.");
             }
+            sc.nextLine();
         }
     }
 
@@ -68,7 +82,5 @@ public class AufgabeMenu {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
         return line;
-
-
     }
 }
