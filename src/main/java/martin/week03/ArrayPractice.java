a534219e828b598d139de4097ab1d109d231d456
Martin <Martin.Widhalm@gmx.at>
2022-09-27 13:55:44 +0200

    Array Übungen 14:00 commit.

--- src/main/java/martin/week03/ArrayPractice.java
+++ src/main/java/martin/week03/ArrayPractice.java
@@ -7,8 +7,10 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class ArrayPractice {
+    private static final Random rand = new Random();
+
     public static void main(String[] args) {
-        /*
+
         MethodenUndSchleifen.head("#", "Number Array");
         int arrayLength1 = UserInput.readUserInputInt(100, -1, "Geben sie bitte die Länge des Arrays an. ");
         System.out.println();
@@ -22,37 +24,50 @@ public class ArrayPractice {
         int[] array3;
         if (arrayChoice == 1) {
             array3 = makeACopy(array1);
+            array1[0] = -100;
+            System.out.printf("Array 1   : %s%n%n", Arrays.toString(array1));
         } else {
             array3 = makeACopy(array2);
+            array2[0] = -100;
+            System.out.printf("Array 2   : %s%n%n", Arrays.toString(array2));
         }
         System.out.printf("Copy Array: %s%n%n", Arrays.toString(array3));
         MethodenUndSchleifen.head("#", "Random Number Array");
         int arrayLength3 = UserInput.readUserInputInt(100, -1, "Geben sie bitte die Länge des Zufalls Arrays an. ");
         System.out.println();
-        int[] arrayRandom1 = randArray(arrayLength3, 0,101);
+        int[] arrayRandom1 = randArray(arrayLength3, 0, 101);
         System.out.printf("Random Array: %s%n%n", Arrays.toString(arrayRandom1));
 
         MethodenUndSchleifen.head("#", "Random Number Array Crazy Range");
         int arrayLength4 = UserInput.readUserInputInt(101, -1, "Geben sie bitte die Länge des Zufalls Arrays an. ");
         System.out.println();
-        int[] arrayRandom2 = randArray(arrayLength4, -50,51);
+        int[] arrayRandom2 = randArray(arrayLength4, -50, 51);
         System.out.printf("Crazy Random Array: %s%n%n", Arrays.toString(arrayRandom2));
 
         MethodenUndSchleifen.head("#", "Random Number Array Min/Max/Avg");
         int arrayLength5 = UserInput.readUserInputInt(100001, -1, "Geben sie bitte die Länge des Zufalls Arrays an. ");
         System.out.println();
-        int[] arrayRandom3 = randArray(arrayLength5, 0,101);
+        int[] arrayRandom3 = randArray(arrayLength5, 0, 101);
+
+        //arrayRandom3 = new int[0];
         System.out.printf("Random Number Array Min/Max/Avg Liste = %s%n", Arrays.toString(arrayRandom3));
-        System.out.printf("Das Minimum ist %d, das Maximum ist %d und der Durchschnitt ist %.2f.%n",getArrayMinimum(arrayRandom3),getArrayMaximum(arrayRandom3),getArrayAverage(arrayRandom3));
-        */
+        System.out.printf("Das Minimum ist %d, das Maximum ist %d und der Durchschnitt ist %.2f.%n", getArrayMinimum(arrayRandom3), getArrayMaximum(arrayRandom3), getArrayAverage(arrayRandom3));
+
 
         MethodenUndSchleifen.head("#", "Bubblesort mit Zahlen");
-        int arrayLength6 = UserInput.readUserInputInt(101, -1, "Geben sie bitte die Länge des Zufalls Arrays an. ");
+        int arrayLength6 = UserInput.readUserInputInt(1001, -1, "Geben sie bitte die Länge des Zufalls Arrays an. ");
         System.out.println();
-        int[] arrayRandom4 = randArray(arrayLength6, 0,101);
+        int[] arrayRandom4 = randArray(arrayLength6, 0, 101);
         System.out.printf("Bubblesort mit Zahlen Liste = %s%n", Arrays.toString(arrayRandom4));
-        int[] arraySorted1 = bubbleSortAlg(arrayRandom4);
+        int[] arraySorted1 = makeACopy(arrayRandom4);
+        algbbubbleSort(arraySorted1);
         System.out.printf("Bubblesort mit Zahlen Liste = %s%n", Arrays.toString(arraySorted1));
+/*
+        MethodenUndSchleifen.head("#", "2D-Array");
+        int arraySize1 = UserInput.readUserInputInt(1001, -1, "Geben sie bitte die Breite des 2D Arrays an. ");
+        int arraySize2 = UserInput.readUserInputInt(1001, -1, "Geben sie bitte die Höhe des 2D Arrays an. ");
+        System.out.println();
+        int[] arrayRandom4 = randArray(arrayLength6, 0,101);*/
     }
 
     public static int[] numberArrayForw(int length) {
@@ -72,13 +87,21 @@ public class ArrayPractice {
     }
 
     public static int[] makeACopy(int[] original) {
-        return original;
+        int[] copy = new int[original.length];
+        try {
+            //for (int j = 0; j < original.length; j++) {copy[j] = original[j];}
+            System.arraycopy(original, 0, copy, 0, original.length);
+            return copy;
+        } catch (ArrayIndexOutOfBoundsException ime) {
+            System.out.println("Adresse ist nicht belegt.");
+        }
+        return copy;
     }
 
     public static int[] randArray(int size, int minRandom, int maxRandom) {
         int[] randArray = new int[size];
         for (int i = 0; i < size; i++) {
-            int randomNumber = new Random().nextInt(maxRandom - minRandom) + minRandom;
+            int randomNumber = rand.nextInt(maxRandom - minRandom) + minRandom;
             randArray[i] = randomNumber;
         }
         return randArray;
@@ -86,12 +109,19 @@ public class ArrayPractice {
 
     public static int getArrayMinimum(int[] arrayName) {
         int min = arrayName[0];
-        for (int j : arrayName) {
-            if (j < min)
-                min = j;
+        try {
+
+            for (int j : arrayName) {
+                if (j < min)
+                    min = j;
+            }
+
+        } catch (ArrayIndexOutOfBoundsException ime) {
+            System.out.println("Adresse ist nicht belegt.");
         }
         return min;
     }
+
 
     public static int getArrayMaximum(int[] arrayName) {
         int max = arrayName[0];
@@ -111,17 +141,15 @@ public class ArrayPractice {
         return avg;
     }
 
-    public static int[] bubbleSortAlg(int[] sortArray) {
-        int[] bubbleArray = sortArray;
+    public static void algbbubbleSort(int[] sortArray) {
         for (int j = 0; j < sortArray.length; j++) {
-            for (int i = 0; i+1 < sortArray.length; i++) {
-                if (bubbleArray[i] > sortArray[i + 1]) {
+            for (int i = 0; i + 1 < sortArray.length; i++) {
+                if (sortArray[i] > sortArray[i + 1]) {
                     int k = sortArray[i];
-                    bubbleArray[i] = sortArray[i + 1];
+                    sortArray[i] = sortArray[i + 1];
                     sortArray[i + 1] = k;
                 }
             }
         }
-        return bubbleArray;
     }
 }
