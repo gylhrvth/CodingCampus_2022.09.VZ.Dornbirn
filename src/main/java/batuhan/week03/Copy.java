cd642d874d684e0a560842f4e13cd146dc1dcf0e
batuhan <batuhandanisment@hotmail.com>
2022-09-27 13:54:21 +0200

    Aufgabe: Aufgabe: Kopie

---
+++ src/main/java/batuhan/week03/Copy.java
@@ -0,0 +1,31 @@
+package batuhan.week03;
+
+import java.util.Arrays;
+
+
+public class Copy {
+    public static void main(String[] args) {
+        int[] arr = {10, 12, 20, 30};
+        System.out.println("arr: " + arr);
+//        System.out.println("arr: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(+arr[i] + " ");
+//            System.out.println();
+//            System.out.println("arr: " + Arrays.toString(arr));
+            System.out.println();
+
+        }
+        System.out.print("Copy" + makeACopy(arr));
+
+    }
+
+    public static String makeACopy(int[] original) {
+        int size = original.length;//zuerst die größe abfragen
+        int[] newarray= new int[size];//neues Array erstellen
+        for (int i = 0; i <size ; i++) {//größe des originals angeben
+            newarray [i]= original[i];//neues Array befüllen mit den werten des Originalen Arrays
+
+        }
+return Arrays.toString(newarray);
+    }
+}
