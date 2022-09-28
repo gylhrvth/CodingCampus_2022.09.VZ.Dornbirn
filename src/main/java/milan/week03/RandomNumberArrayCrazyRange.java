70940411de0737bcf6f6f0509e16c043133e2c01
mailandr <splintersfinest@gmail.com>
2022-09-28 11:15:04 +0200

    Random Number Array Crazy Range gelöst.

--- src/main/java/milan/week03/RandomNumberArrayCrazyRange.java
+++ src/main/java/milan/week03/RandomNumberArrayCrazyRange.java
@@ -14,6 +14,8 @@ public class RandomNumberArrayCrazyRange {
         int[] template = new int[10];
         for (int i = 0; i < template.length; i++) {
             int rdm = ran.nextInt(101) - 50; // random bound geht immer von 0 aus, mit -50 wird der Bound um -50 verschoben
+            //int rdm = ran.nextInt(-50,50); // funktioniert auch?!
+
             template[i] = rdm;
         }
 
