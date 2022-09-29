830500f658f619b293cff9d7ce065f575af6c1dd
mase52 <cajic.ma@gmail.com>
2022-09-29 11:43:36 +0200

    Array counting

--- src/main/java/mase/week03/CrazyRange.java
+++ src/main/java/mase/week03/CrazyRange.java
@@ -3,9 +3,25 @@ package mase.week03;
 import java.util.Random;
 
 public class CrazyRange {
-    Random random = new Random();
+    public static Random random = new Random();
+
     public static void main(String[] args) {
+        int[] arr = new int[10];
 
+        for (int i = 0; i < arr.length; i++) {
+            int randomNumber = random.nextInt(101) - 50;
+            arr[i] = randomNumber;
+        }
+        System.out.print("[");
+        boolean firstLine = true;
+        for (int value : arr) {
+            if (firstLine) {
+                firstLine = false;
+            } else {
+                System.out.print(value + ", ");
+            }
+            System.out.print(value);
+        }
+        System.out.print("]");
     }
-
 }
