a0eba84e41555f8e290b0418ad5f7fd98b2a6278
Alp <alpsahin60@outlook.com>
2022-10-03 12:26:21 +0200

    Arrays- ArrayUebungen ArrayMax Min Avarage

---
+++ src/main/java/alp/week04/bubblesortmitString.java
@@ -0,0 +1,19 @@
+package alp.week04;
+
+public class bubblesortmitString {
+
+    public static void main(String[] args) {
+        String[] name =new String[]{"Viktor", "Batuhan", "Milan", "Stefan", "Berna", "Franky", "Fatima", "Mike", "Mase", "Patric", "Alp", "Gyula", "Lukas", "Svitlana"};
+        for (int i = 0; i < name.length; i++) {
+            for (int j = i +1; j < name.length ; j++) {
+                if (name[j].compareTo(name[i]) < 0){
+                    String wort = name[i];
+                    name[i] = name[j];
+                    name[j] = wort;
+                }
+            }
+            System.out.println(name[i]);
+        }
+
+    }
+}
