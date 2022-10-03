b41a36796e515ea84eac6b6ad536b6ce1c1e14e7
Alp <alpsahin60@outlook.com>
2022-10-03 14:00:03 +0200

    Arrays- ArrayUebungen ArrayMax Min Avarage

---
+++ src/main/java/alp/week04/BubbleSortMitStringsExtended.java
@@ -0,0 +1,49 @@
+package alp.week04;
+
+import java.util.Arrays;
+
+public class BubbleSortMitStringsExtended {
+
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"Viktor", "Batuhan", "Milan", "Stefan", "Berna", "Franky", "Fatima", "Mike", "Mase", "Patric", "Alp", "Gyula", "Lukas", "Svitlana"};
+        System.out.println("Before: " + Arrays.toString(names));
+        sortBubbleAlphabetically(names);
+        System.out.println("After : " + Arrays.toString(names));
+        sortBubbleAlphabeticallyBool(names, false);             //wenn false ist sortiert er von z-a     //wenn true ist sortiert er von a-z
+        System.out.println("Bool  : " + Arrays.toString(names));
+    }
+
+
+    public static void sortBubbleAlphabetically(String[] values) {
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values.length - i - 1; j++) {
+                if (values[j].compareTo(values[j + 1]) > 0) {
+                    String temp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortBubbleAlphabeticallyDescending(String[] values) {
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values.length - i - 1; j++) {
+                if (values[j].compareTo(values[j + 1]) < 0) {
+                    String temp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortBubbleAlphabeticallyBool(String[] values, boolean Alphabetically) {
+        if (Alphabetically) {
+            sortBubbleAlphabetically(values);
+        } else {
+            sortBubbleAlphabeticallyDescending(values);
+        }
+    }
+}
