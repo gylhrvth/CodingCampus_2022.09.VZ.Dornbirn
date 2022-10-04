825a07fa544e8232c746611f2d6b93bc02ee3452
PatricBoso <mr.patric.boso@gmail.com>
2022-10-04 15:05:03 +0200

    041022

--- src/main/java/patric/week04/Reverse.java
+++ src/main/java/patric/week04/Reverse.java
@@ -1,20 +1,42 @@
 package patric.week04;
 
+import java.util.Scanner;
+
 public class Reverse {
 
     public static void main(String[] args) {
-        Split();
-
+        methode();
     }
+    public static void methode() {
+        System.out.println("Enter string to reverse:");
 
-    public static void Split() {
+        Scanner read = new Scanner(System.in);
+        String str = read.nextLine();
+        String reverse = "";
 
-        String source = "Alles verdreht!";
 
-        for (String part : source.split(" ")) {
-            System.out.print(new StringBuilder(part).reverse());
-            System.out.print(" ");
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
         }
+
+        System.out.println("Reversed string is:");
+        System.out.println(reverse);
     }
+
 
 }
+
+
+
+
+//    public static void Split() {
+//
+//        String source = "!!!!Alles***verdreht!!!!";
+//
+//        for (String part : source.split(" ")) {
+//            System.out.print(new StringBuilder(part).reverse());
+//            System.out.print(" ");
+//        }
+//    }
+
+
