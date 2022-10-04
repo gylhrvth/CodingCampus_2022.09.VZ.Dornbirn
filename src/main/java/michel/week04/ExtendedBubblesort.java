4f2d8cb9c9cb36755b75118fd9a27dee29be3dcb
mase52 <cajic.ma@gmail.com>
2022-10-04 08:35:00 +0200

    Bubblesort & String

--- src/main/java/michel/week04/ExtendedBubblesort.java
+++ src/main/java/michel/week04/ExtendedBubblesort.java
@@ -1,16 +1,17 @@
-package mase.week04;
+package michel.week04;
 
 import java.util.Arrays;
 
-public class StringBubblesort {
+public class ExtendedBubblesort {
     public static void main(String[] args) {
         String[] names = new String[]{"Viktor", "Batuhan", "Milan", "Stefan", "Berna", "Franky", "Fatima", "Mike", "Mase", "Patric", "Alp", "Gyula", "Lukas", "Svitlana"};
         bubbleSort(names, true);
         System.out.println(Arrays.toString(names));
-
+        bubbleSort(names, false);
+        System.out.println(Arrays.toString(names));
     }
 
-    public static void bubbleSort(String[] values, boolean isAscending) {
+    public static void bubbleSort(String[] values, boolean isAscending){
         if (isAscending) {
             bubbleSortAsc(values);
         } else {
@@ -20,18 +21,17 @@ public class StringBubblesort {
 
     public static void bubbleSortAsc(String[] names) {
         String value;
-
         for (int i = 0; i < names.length; i++) {
             for (int j = i + 1; j < names.length; j++) {
-                if (names[j].compareTo(names[i]) < 0) {
-                    value = names[j];
-                    names[j] = names[i];
-                    names[i] = value;
+                for (int k = 0; k < names.length; k++) {
+                    if (names[j].compareTo(names[i]) < 0) {
+                        value = names[j];
+                        names[j] = names[i];
+                        names[i] = value;
+                    }
                 }
             }
         }
-        System.out.println();
-
     }
 
     public static void bubbleSortDesc(String[] names) {
@@ -43,10 +43,8 @@ public class StringBubblesort {
                     names[i] = names[j];
                     names[j] = value;
                 }
+
             }
         }
-
     }
-
-
 }
