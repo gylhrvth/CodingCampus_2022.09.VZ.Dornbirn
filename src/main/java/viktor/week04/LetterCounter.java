f34ebd82552190b9940b474bf6d92e1856d8a96b
Viktor <selm.viktor@gmail.com>
2022-10-05 15:24:51 +0200

    String - LetterCounter is fixed!

---
+++ src/main/java/viktor/week04/LetterCounter.java
@@ -0,0 +1,62 @@
+package viktor.week04;
+
+import lukas.data.Texts;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class LetterCounter {
+
+    public static int[] counterLetter(String text) {
+
+        int[] count = new int[Character.MAX_VALUE + 1];
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char c = text.charAt(i);
+
+            count[c]++;
+        }
+
+        return count;
+
+    }
+
+    public static void print(int[] count) {
+
+        for (int j = 0; j < count.length; j++) {
+
+
+            if (count[j] > 0) {
+
+                String charName;
+                if (j < 33) {
+                    charName = Character.getName(j);
+                } else {
+
+                    charName = Character.toString((char)j);
+                }
+                System.out.printf("%5d | (%5d) %s%n", count[j], j, charName);
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String text = Texts.getSimpleText();
+        System.out.println(text);
+        System.out.println();
+
+        String charName = Character.toString(48);
+        System.out.println(charName);
+
+        System.out.println(Character.MAX_VALUE+1);
+
+
+        int[] count = counterLetter(text);
+        print(count);
+
+
+    }
+}
