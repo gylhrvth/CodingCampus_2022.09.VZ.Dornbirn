d453af72d98ced036d26c90f8b0cac551e78fa92
batuhan <batuhandanisment@hotmail.com>
2022-10-06 15:27:48 +0200

    hesse2

--- src/main/java/batuhan/week04/Hesse.java
+++ src/main/java/batuhan/week04/Hesse.java
@@ -2,6 +2,8 @@ package batuhan.week04;
 
 import lukas.data.Texts;
 
+import java.util.Scanner;
+
 public class Hesse {
     public static void main(String[] args) {
 
@@ -9,21 +11,24 @@ public class Hesse {
         String text = Texts.HESSE;
         System.out.println(text);
         printCharacters(text);
+        String upcase = text.replace("Hesse", "HESSE");
+
+
 
 
     }
 
-    public static void printCharacters(String str){
-        int counter =0;
+    public static void printCharacters(String str) {
+        int counter = 0;
         String[] arr = str.split(" ");
-        for (int i = 0; i <arr.length; i++) {
-            if (arr[i].contains("Hesse") ){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].contains("Hesse")) {
                 counter++;
 
 
             }
 
         }
-        System.out.println("Das Wort Hesse wurde: "+counter +" gefunden!");
+        System.out.println("Das Wort Hesse wurde: " + counter + " gefunden!");
     }
 }
