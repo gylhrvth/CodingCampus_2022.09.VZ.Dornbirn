96d5feffe35c25419bf832508941af5dae1198c0
mailandr <splintersfinest@gmail.com>
2022-10-06 12:22:36 +0200

    SucheHesse erstellt

---
+++ src/main/java/milan/week04/StringHesse.java
@@ -0,0 +1,31 @@
+package milan.week04;
+
+import lukas.data.Texts;
+
+public class StringHesse {
+
+    public static void main(String[] args) {
+
+        String text = Texts.HESSE;
+        String[] splitText = text.split("\\s");
+        int anzahl = countHesse(splitText);
+
+        System.out.println();
+        System.out.println("Hesse kommt " + anzahl + "mal vor!");
+
+
+    }
+
+    public static int countHesse(String[] text){
+        int count = 0;
+        for (int i = 0; i < text.length; i++) {
+            if (text[i].contains("Hesse")) {
+                count++;
+                System.out.println("Hesse an folgender Position: " + i);
+            }
+
+        }
+        return count;
+    }
+
+}
