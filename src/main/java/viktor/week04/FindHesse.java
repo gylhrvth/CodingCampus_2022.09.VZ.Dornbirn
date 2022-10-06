9fe6be673ffee7ba672245912d08aeb763e1addf
Viktor <selm.viktor@gmail.com>
2022-10-06 11:10:30 +0200

    String - Count "Hesse" (another idea with regular expression)

--- src/main/java/viktor/week04/FindHesse.java
+++ src/main/java/viktor/week04/FindHesse.java
@@ -2,6 +2,10 @@ package viktor.week04;
 
 import lukas.data.Texts;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FindHesse {
 
 
@@ -18,12 +22,26 @@ public class FindHesse {
 
             if (stringText[i].contains("Hesse") == b) {
                 count++;
-                System.out.println(" Hesse's index: " + i);
+                System.out.println(" Hesse's wordindex: " + i);
 
             }
         }
         System.out.println();
         System.out.println("Hesse comes " + count + " times for!");
+
+
+        //Indexof
+
+
+        Pattern pattern = Pattern.compile("Hesse ([a-zA-Zßüöä]+)");
+        Matcher matcher = pattern.matcher(text);
+        int count2 = 0;
+        while(matcher.find()) {
+            System.out.println(matcher.start());
+            System.out.println(matcher.group(1));
+            count2++;
+        }
+        System.out.println(count2);
 
     }
 }
