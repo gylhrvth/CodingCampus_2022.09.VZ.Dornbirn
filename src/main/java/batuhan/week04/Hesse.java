6648ac63868856ba0e35ce5f84fa00ef9fab4ebf
batuhan <batuhandanisment@hotmail.com>
2022-10-07 10:32:19 +0200

    hesse

--- src/main/java/batuhan/week04/Hesse.java
+++ src/main/java/batuhan/week04/Hesse.java
@@ -2,33 +2,41 @@ package batuhan.week04;
 
 import lukas.data.Texts;
 
-import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Hesse {
     public static void main(String[] args) {
 
 
         String text = Texts.HESSE;
-        System.out.println(text);
-        printCharacters(text);
-        String upcase = text.replace("Hesse", "HESSE");
+//        String upcase = text.replace("Hesse", "HESSE");
+        String[] upcase = text.split("\\s");//Wandelt jedes Wort im Text in ein Array um
 
+//        printCharacters(text);
 
+        for (int i = 0; i < upcase.length; i++) {
+           upcase[i] = upcase[i].replace("Hesse", "HESSE");
 
-
-    }
-
-    public static void printCharacters(String str) {
-        int counter = 0;
-        String[] arr = str.split(" ");
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].contains("Hesse")) {
-                counter++;
-
-
-            }
 
         }
-        System.out.println("Das Wort Hesse wurde: " + counter + " gefunden!");
+
+
+        System.out.println(Arrays.toString(upcase));
+
     }
+
+//    public static void printCharacters(String str) {
+//        int counter = 0;
+//        String[] arr = str.split(" ");
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i].contains("Hesse")) {
+//                counter++;
+//
+//
+//            }
+//
+//        }
+//        System.out.println("Das Wort Hesse wurde: " + counter + " gefunden!");
+//    }
 }
