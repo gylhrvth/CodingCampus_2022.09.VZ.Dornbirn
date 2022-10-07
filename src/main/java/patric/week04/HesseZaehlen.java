8ada0c4d7328553ed78b2ffb9e62216300916734
pabos <mr.patric.boso@gmail.com>
2022-10-07 10:03:39 +0200

    Änderung Rechtschreibung

--- src/main/java/patric/week04/HesseZaehlen.java
+++ src/main/java/patric/week04/HesseZaehlen.java
@@ -32,7 +32,7 @@ public class HesseZaehlen {
         }
 
         System.out.println("Das Wort \"" + wort + "\" ist " + counter + " mal enthalten. ");
-        System.out.println("Befindet sich an der Stelhfdhghlle");
+        System.out.println("Befindet sich an der Stelle");
 
 
         printWordIndex(text, wort);
