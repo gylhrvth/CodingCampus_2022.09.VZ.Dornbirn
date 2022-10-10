6587870ba749e743779dd1abe9e51234dc7abff8
Mike <suter.johann.ms@gmail.com>
2022-10-10 10:44:56 +0200

    Day of Birthday

--- src/main/java/michel/week05/DayOfBirthday.java
+++ src/main/java/michel/week05/DayOfBirthday.java
@@ -4,28 +4,32 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class DayOfBirthday {
     public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        GregorianCalendar date = new GregorianCalendar();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
-
-        String birthdayDate = userInputBrithday();
-        try {
-            Date myDate = sdf.parse(birthdayDate);
-            date.setTime(myDate);
-            System.out.println(sdf.format(date.getTime()));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.println(birthdayDate);
-    }
-
-    public static String userInputBrithday() {
-        System.out.println("Bitte gib dein Gebrutsdatum im Format yyyy.MM.dd ein:");
-        String userInput = sc.nextLine();
-        return userInput;
-    }
+//        GregorianCalendar date = new GregorianCalendar();
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+//        System.out.println(date.get(Calendar.DAY_OF_WEEK));
+//        date.set(Calendar.DAY_OF_WEEK,1);
+//
+//        String birthdayDate = userInputBrithday();
+//        try {
+//            Date myDate = sdf.parse(birthdayDate);
+//            date.setTime(myDate);
+//            System.out.println(sdf.format(date.getTime()));
+//        } catch (ParseException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        System.out.println(birthdayDate);
+//    }
+//
+//    public static String userInputBrithday() {
+//        System.out.println("Bitte gib dein Gebrutsdatum im Format yyyy.MM.dd ein:");
+//        String userInput = sc.nextLine();
+//        return userInput;
+//    }
 }
