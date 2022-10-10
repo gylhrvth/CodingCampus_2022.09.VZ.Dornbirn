264f0e451a218d1d80ed6e5a27dc8b79dd18d612
Mike <suter.johann.ms@gmail.com>
2022-10-10 11:06:43 +0200

    Day of Birthday

--- src/main/java/michel/week05/DayOfBirthday.java
+++ src/main/java/michel/week05/DayOfBirthday.java
@@ -3,6 +3,7 @@ package michel.week05;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.util.Calendar;
 
@@ -10,26 +11,27 @@ public class DayOfBirthday {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-//        GregorianCalendar date = new GregorianCalendar();
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        GregorianCalendar date = new GregorianCalendar();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
 //        System.out.println(date.get(Calendar.DAY_OF_WEEK));
 //        date.set(Calendar.DAY_OF_WEEK,1);
-//
-//        String birthdayDate = userInputBrithday();
-//        try {
-//            Date myDate = sdf.parse(birthdayDate);
-//            date.setTime(myDate);
+
+        String birthdayDate = userInputBrithday();
+        try {
+            Date myDate = sdf.parse(birthdayDate);
+            date.setTime(myDate);
 //            System.out.println(sdf.format(date.getTime()));
-//        } catch (ParseException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        System.out.println(birthdayDate);
-//    }
-//
-//    public static String userInputBrithday() {
-//        System.out.println("Bitte gib dein Gebrutsdatum im Format yyyy.MM.dd ein:");
-//        String userInput = sc.nextLine();
-//        return userInput;
-//    }
+            System.out.print("Wochentag: ");
+            System.out.print(date.get(Calendar.DAY_OF_WEEK) + " - ");
+            System.out.println(date.getTime());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String userInputBrithday() {
+        System.out.println("Bitte gib dein Gebrutsdatum im Format yyyy.MM.dd ein:");
+        String userInput = sc.nextLine();
+        return userInput;
+    }
 }
