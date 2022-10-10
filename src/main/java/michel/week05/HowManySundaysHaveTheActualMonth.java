2628c95eba19a58ec754dbe20996eec5d5b4c1fa
Mike <suter.johann.ms@gmail.com>
2022-10-10 15:20:40 +0200

    GIT cleanup

--- src/main/java/michel/week05/HowManySundaysHaveTheActualMonth.java
+++ src/main/java/michel/week05/HowManySundaysHaveTheActualMonth.java
@@ -1,21 +1,17 @@
 package michel.week05;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
-
 
 public class HowManySundaysHaveTheActualMonth {
     public static void main(String[] args) {
         int countSundays = getSundays(2022, 10);
-        System.out.println("Der angegebene Monat hat " + countSundays + " Sonntage!");
+        System.out.println("Der angegebene Monat hat > " + countSundays + " < Sonntage!");
     }
 
     private static int getSundays(int year, int month) {
         GregorianCalendar startDay = new GregorianCalendar(year, month - 1, 1);
         GregorianCalendar endDay = new GregorianCalendar(year, month, 1);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.");
         int counter = 0;
 
         while (!startDay.equals(endDay)) {
