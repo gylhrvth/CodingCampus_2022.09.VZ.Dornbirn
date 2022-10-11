e880ed4a36faec28f8eb207a4b70115fc8100f5a
pabos <mr.patric.boso@gmail.com>
2022-10-11 13:40:19 +0200

    111022

--- src/main/java/patric/week05/DateienZaehlenUndSummen.java
+++ src/main/java/patric/week05/DateienZaehlenUndSummen.java
@@ -2,17 +2,26 @@ package patric.week05;
 
 import java.io.File;
 
-import static patric.week05.VerzeichnisAuflisten.printFiles;
-import static patric.week05.VerzeichnisAuflisten.sortFiles;
 
 public class DateienZaehlenUndSummen {
     public static void main(String[] args) {
         File f = new File("C:\\Users\\pabos\\IdeaProjects\\CodingCampus_2022.09.VZ.Dornbirn\\src\\main\\java\\patric");
-        printFiles(sortFiles(f), f);
+        printFileInformation(f);
     }
 
+    public static void printFileInformation(File parent) {
+        int summ = 0;
+        if (parent.exists() && parent.isDirectory()) {
+            File[] child = parent.listFiles();
 
-
+            for (int i = 0; i < child.length; i++) {
+                System.out.printf("%6d %s\n", child[i].length(), child[i].getName());
+                summ += child[i].length();
+            }
+            System.out.println("Anzahl der Dateien im Verzeichnis:    " + child.length);
+            System.out.println("Gesamtsumme der Groesse der Dateien " + summ);
+        }
+    }
 
 
 }
