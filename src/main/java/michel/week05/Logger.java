bf5293c2b33fa1cd6e06f3fcb78119c1e2f349bc
Mike <suter.johann.ms@gmail.com>
2022-10-13 11:10:10 +0200

    finished

---
+++ src/main/java/michel/week05/Logger.java
@@ -0,0 +1,45 @@
+package michel.week05;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class Logger {
+    public static int ERROR = 1;
+    public static int WARNING = 2;
+    public static int INFO = 3;
+
+    public static void log(int severity, String message) {
+        GregorianCalendar calendar = new GregorianCalendar();
+        SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd.");
+        SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
+//        File loggold = new File("/Users/sutez/IdeaProjects/CodingCampus_2022.09.VZ.Dornbirn/assets/tmp/loggFile.old.txt");
+
+
+
+        try {
+            File f = new File("assets/tmp/loggFile.txt");
+            f.getParentFile().mkdirs();
+
+            PrintStream ps = new PrintStream(new FileOutputStream(f, true));
+            ps.print(date.format(calendar.getTime()) + " " + (time.format(calendar.getTime())) + "  ");
+            if (severity == ERROR) {
+                ps.print("ERROR: ");
+            } else if (severity == WARNING) {
+                ps.print("WARNING: ");
+            } else if (severity == INFO) {
+                ps.print("INFO: ");
+            }
+            ps.println(message);
+
+            ps.close();
+
+        } catch (IOException ioe) {
+            System.out.println("Ich kann nicht schreiben");
+            ioe.printStackTrace();
+        }
+    }
+}
