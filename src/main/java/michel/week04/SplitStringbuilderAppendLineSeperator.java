d6eece371803564849684a330d0a023b56a1293d
Mike <suter.johann.ms@gmail.com>
2022-10-17 12:31:46 +0200

    rdy 4 action

--- src/main/java/michel/week04/SplitStringbuilderAppendLineSeperator.java
+++
@@ -1,9 +0,0 @@
-package michel.week04;
-
-public class SplitStringbuilderAppendLineSeperator {
-    public static void main(String[] args) {
-//        String text = ("Heute wird ein guter Tag! Heute wird der beste Tag überhaupt! Heute wird ein spitzen super Tag!");
-//        String [] words = wordSplit(text,"Tag");
-
-    }
-}
