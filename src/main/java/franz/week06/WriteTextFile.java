a8ee8b3b9fe68492888913d029d613f0ac97a940
Fanz <franzwinkler@hotmail.com>
2022-10-19 10:06:41 +0200

    Git Clean-up

---
+++ src/main/java/franz/week06/WriteTextFile.java
@@ -0,0 +1,27 @@
+package franz.week06;
+
+import gyula.week05.ReadFileFromRessorcesWithBufferedReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Objects;
+
+public class WriteTextFile {
+    public static void main(String[] args) {
+        try {
+            Reader reader = new InputStreamReader(
+                    Objects.requireNonNull(
+                            ReadFileFromRessorcesWithBufferedReader.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")));
+            BufferedReader br = new BufferedReader(reader);
+            while (br.ready()) {
+                String line = br.readLine();
+                System.out.println(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
