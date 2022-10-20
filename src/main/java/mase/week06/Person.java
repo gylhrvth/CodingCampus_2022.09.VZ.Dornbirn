22536e892e6ab51afbdd2f0d0430e846e006f92c
mase52 <cajic.ma@gmail.com>
2022-10-20 10:15:24 +0200

    Object oriented Programming

---
+++ src/main/java/mase/week06/Person.java
@@ -0,0 +1,27 @@
+package mase.week06;
+
+public class Person {
+    public double size;
+    public int age;
+    public String name;
+    public float weight;
+
+public Person(String name,int age,double size,float weight){
+    this.name = name;
+    this.age = age;
+    this.size = size;
+    this.weight = weight;
+}
+
+public int getAge(){
+    return age;
+}
+public void setAge(int age){
+    this.age = age;
+
+}
+public String toString(){
+    return "Mein Name ist " + name + " Ich bin " + age + " Jahre alt " + "Ich bin " + size + " groß, " + "und wiege " + weight + "kg";
+}
+
+}
