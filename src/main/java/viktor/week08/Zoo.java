e7a8d192e4152380b83882792b33201248008c3c
Viktor <selm.viktor@gmail.com>
2022-11-02 17:52:12 +0100

    Zoo-Structure with enclosures and animals

--- src/main/java/viktor/week08/Zoo.java
+++ src/main/java/viktor/week08/Zoo.java
@@ -1,2 +1,44 @@
-package viktor.week08;public class Zoo {
+package viktor.week08;
+
+import batuhan.week03.Array;
+
+import java.util.ArrayList;
+
+public class Zoo {
+
+    private String name;
+    private int yearOfFounding;
+
+
+    ArrayList<Gehege> list = new ArrayList<Gehege>();
+
+    public Zoo(String name, int yearOfFounding) {
+        this.name = name;
+        this.yearOfFounding = yearOfFounding;
+    }
+
+    public void putGehegeToZoo(Gehege d) {
+        list.add(d);
+    }
+
+    public void removeGehegeFromZoo(Gehege d) {
+        list.remove(d);
+    }
+
+    public void printListWithEnclosures() {
+
+        System.out.println("|----Zoo: " + name + ", gegründet " + yearOfFounding);
+        for (Gehege g : list) {
+            g.printGehege();
+
+        }
+    }
+
+    public String toString(){
+        return "|----Zoo: " + name + ", gegründet " + yearOfFounding;
+    }
 }
+
+
+
+
--- src/main/java/viktor/week08/ZooSimulation.java
+++ src/main/java/viktor/week08/ZooSimulation.java
@@ -1,2 +1,45 @@
-package viktor.week08;public class ZooSimulation {
+package viktor.week08;
+
+public class ZooSimulation {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n");
+
+        Zoo myZoo = new Zoo("Tiergarten Dornbirn",2022);
+
+        Gehege myGehege1 = new Gehege("Alpenwiese");
+        Gehege myGehege2 = new Gehege("Ried");
+        Gehege myGehege3 = new Gehege("Terrarium (warm)");
+
+        myZoo.putGehegeToZoo(myGehege1);
+        myZoo.putGehegeToZoo(myGehege2);
+        myZoo.putGehegeToZoo(myGehege3);
+        myZoo.putGehegeToZoo(new Gehege("Feldkirch Wildpark"));
+
+        myZoo.removeGehegeFromZoo(myGehege2);
+        myZoo.putGehegeToZoo(new Gehege("Ried"));
+
+        Tier tier1 = new Tier("Alfred","Storch");
+        Tier tier2 = new Tier("Hugo","Storch");
+        Tier tier3 = new Tier("Rijksa","Kuh");
+
+        myGehege1.putTierToGehege(tier1);
+        myGehege1.putTierToGehege(tier3);
+
+        myGehege3.putTierToGehege(tier2);
+
+        myZoo.printListWithEnclosures();
+        System.out.println("\n");
+        //Test des Löschens der Tiere
+        myGehege1.removeTierFromGehege(tier1);
+        myGehege1.removeTierFromGehege(tier3);
+        myZoo.printListWithEnclosures();
+
+
+
+
+
+
+    }
 }
