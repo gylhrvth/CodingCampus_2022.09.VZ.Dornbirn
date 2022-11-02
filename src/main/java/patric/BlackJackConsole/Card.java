3ca9bc67c859ebbab93d418adccc5bf851a3e29f
pabos <mr.patric.boso@gmail.com>
2022-11-02 10:05:01 +0100

    021122

---
+++ src/main/java/patric/BlackJackConsole/Card.java
@@ -0,0 +1,31 @@
+package patric.BlackJackConsole;
+
+public class Card {
+
+    //vars
+    private Suit suit;
+    private Rank rank;
+
+    //create a card given a suit and a rank
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public int getValue() {
+        return rank.rankValue;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public String toString() {
+        return ("[" + rank + " of " + suit + "] (" + this.getValue() + ")");
+    }
+
+}
---
+++ src/main/java/patric/BlackJackConsole/Game.java
@@ -0,0 +1,17 @@
+package patric.BlackJackConsole;
+
+public class Game {
+
+    //Create variables used by the Game class
+    private int wins, losses, pushes;
+
+    //Constructor for the Game class
+    public Game() {
+
+        //Set score to 0
+        wins = 0;
+        losses = 0;
+        pushes = 0;
+    }
+
+}
