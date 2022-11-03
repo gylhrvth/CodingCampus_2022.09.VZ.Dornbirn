b45bfe3476d6077bb08899b3382ec29361995187
Mike <suter.johann.ms@gmail.com>
2022-11-03 18:59:01 +0100

    updated

---
+++ src/main/java/michel/week08/Tiergarten/Enclosure.java
@@ -0,0 +1,43 @@
+package michel.week08.Tiergarten;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Enclosure {
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+    private List<Animal> animalList =new ArrayList<>();
+
+    public Enclosure(String description){
+        this.description=description;
+    }
+
+    public void addAnimal(Animal animal){
+        animalList.add(animal);
+    }
+    public void removeAnimal(Animal animal){
+        animalList.remove(animal);
+    }
+    public void printEnclosure(){
+        System.out.println( "|   |--- Gehege: " + description);
+        if(animalList.size()>0) {
+            for (Animal a : animalList) {
+                System.out.println("|        |--- " + a.toString());
+            }
+        }else{
+            System.out.println("|        |--- " + "(leer)");
+        }
+    }
+    @Override
+    public String toString() {
+       return description;
+    }
+}
+
