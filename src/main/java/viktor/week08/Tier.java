1a40de95ec92827922483c92b3a360feaa23cada
Viktor <selm.viktor@gmail.com>
2022-11-04 11:11:10 +0100

    New class Pfleger

--- src/main/java/viktor/week08/Tier.java
+++ src/main/java/viktor/week08/Tier.java
@@ -1,14 +1,20 @@
 package viktor.week08;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tier {
 
     private String name;
     private String gattung;
+    private double quantity;
+
 
     public Tier(String name, String gattung) {
         this.name = name;
         this.gattung = gattung;
     }
+
 
     @Override
     public String toString() {
--- src/main/java/viktor/week08/Zoo.java
+++ src/main/java/viktor/week08/Zoo.java
@@ -3,14 +3,18 @@ package viktor.week08;
 import batuhan.week03.Array;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Zoo {
 
     private String name;
     private int yearOfFounding;
+    private Futter feed;
 
 
     ArrayList<Gehege> list = new ArrayList<Gehege>();
+    Map<String, Double> foods = new HashMap<String, Double>();
 
     public Zoo(String name, int yearOfFounding) {
         this.name = name;
@@ -34,7 +38,21 @@ public class Zoo {
         }
     }
 
-    public String toString(){
+    public void putFoodToAnimal(Futter food, double quantity) {
+        foods.put(food.getName(), quantity);
+    }
+
+
+    public void showAllFoods() {
+
+        for (String i : foods.keySet()) {
+            System.out.println(i + " " + foods.get(i));
+        }
+    }
+
+
+
+    public String toString() {
         return "|----Zoo: " + name + ", gegründet " + yearOfFounding;
     }
 }
