6728209ac4e0a93ef256aee9983ec29abbd818c6
mailandr <splintersfinest@gmail.com>
2022-11-14 15:23:23 +0100

    OOP Car extended

--- src/main/java/milan/week09/carExtended/RepairStation.java
+++ src/main/java/milan/week09/carExtended/RepairStation.java
@@ -7,26 +7,18 @@ public class RepairStation {
 
     public static Random ran = new Random();
     private String name;
-    private Vector<Motor> motorenListe = new Vector<>();
-    private Vector<Tank> tankListe = new Vector<>();
-
 
     public RepairStation(String name) {
         this.name = name;
     }
 
     public void changeEnginge(Car kunde){
-        kunde.setMotor(motorenListe.get(ran.nextInt(motorenListe.size())));
+        Motor altMotor = kunde.getMotor();
+        Motor motor = altMotor.clone();
+        kunde.setMotor(motor);
     }
     public String getName() {
         return name;
     }
 
-    public void addTankToList(Tank tank) {
-        tankListe.add(tank);
-    }
-
-    public void addMotorToList(Motor motor) {
-        motorenListe.add(motor);
-    }
 }
