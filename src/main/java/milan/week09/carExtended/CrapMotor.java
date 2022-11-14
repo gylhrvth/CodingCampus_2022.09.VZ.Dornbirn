0d7ef37106dd89c1fe0334c38177897cba0435f1
mailandr <splintersfinest@gmail.com>
2022-11-14 14:27:52 +0100

    OOP Car extended

---
+++ src/main/java/milan/week09/carExtended/CrapMotor.java
@@ -0,0 +1,17 @@
+package milan.week09.carExtended;
+
+public class CrapMotor extends Motor {
+
+    public CrapMotor(String model, int leistung) {
+        super(model, leistung);
+    }
+
+
+    public void statusUpdate() {
+        if (kmStand > ran.nextInt(15000000)) {
+            setStatusNotDefect(false);
+            System.out.println("Achtung!!!! Motorschaden nach " + kmStand + " km.\n" + "!!!Bitte zur RepairStation gehen!!!");
+        }
+    }
+}
+
