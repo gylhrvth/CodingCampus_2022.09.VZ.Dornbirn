6fda82a5c1b42eff559b82797eaebc5335bf9dfc
batuhan <batuhandanisment@hotmail.com>
2022-11-15 15:25:12 +0100

    Carsimulation

--- src/main/java/batuhan/wwek08/car/Engine.java
+++ src/main/java/batuhan/wwek08/car/Engine.java
@@ -2,24 +2,37 @@ package batuhan.wwek08.car;
 
 public class Engine {
     private String motor;
-    private boolean startStop;
+    private boolean starEngine;
+    private boolean stopEngine;
+    private int kw;
 
 
 
-    public Engine(String motor) {
+    public Engine(String motor,int kw) {
         this.motor = motor;
+        starEngine = true;
+        stopEngine = false;
+        this.kw=kw;
+
 
     }
 
-  public boolean StartStop(){
-        if (true){
-            System.out.println("Das Auto wird gestartet!\n");
+    public int getKw() {
+        return kw;
+    }
 
-        }
-        else {
-            System.out.println("Das Auto wird abgeschaltet!\n");
-        }
-  return StartStop();
+    public void setStarEngine(boolean starEngine) {
+        this.starEngine = starEngine;
+    }
+
+    public void setStopEngine(boolean stopEngine) {
+        this.stopEngine = stopEngine;
+    }
+    public boolean getStartEngine(){
+        return  starEngine;
+    }
+    public boolean getStopEngine(){
+        return  stopEngine;
     }
 
     public String getMotor() {
@@ -29,6 +42,14 @@ public class Engine {
     public String toString(){
 
         return "Ein neuer " + motor + " wurde eingebaut\n";
+    }
+    public void istmeinMotoran(){
+        if (getStartEngine()){
+            System.out.println("Motor ist an.");
+        }
+        else {
+            System.out.println("Motor ist aus");
+        }
     }
 
 }
