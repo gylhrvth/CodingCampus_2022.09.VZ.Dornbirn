d0c8d1a41e50d79ee57a55e57dc9523abcc7a44b
Fanz <franzwinkler@hotmail.com>
2022-11-16 14:10:53 +0100

    Bug Fix

--- src/main/java/franz/week08/Cars/Garage.java
+++ src/main/java/franz/week08/Cars/Garage.java
@@ -7,20 +7,39 @@ public class Garage {
     public static void main(String[] args) {
         Vector<Car> mygarage = new Vector<>();
 
-        mygarage.add(new Car("Mazda", "3", 135, 45, "Diesel", 1250));
-        mygarage.add(new Car("VW", "Touran", 105, 55, "Diesel", 1520));
+
+        mygarage.add(new Car("Mazda", "3", 45, "Diesel", 1250, new Engine("v8", 100
+        )));
+        mygarage.add(new SelfRepairingCar("VW", "Touran", 55, "Diesel", 1520, new Engine("w12", 77)));
+
+        mygarage.add(new AeroDynamicCar("Dacia", "Logan", 50, "Benzin", 1250, new Engine("v6", 200)));
+        mygarage.add(new CrapCar("Mercedes", "X350d", 75, "Elektro", 1700, new Engine("E- Motor", 250)));
 
         for (Car c : mygarage) {
             System.out.println(c.getHersteller() + "  " + c.getModel());
-            while (c.getAktuellerTankFuellstand() > 0) {
-                int drivenKM = c.drive(145);
-                if (drivenKM != 145) {
-                    System.out.println("Dein Tank ist leer; GEHE tanken! Du bist " + drivenKM + " km gefahren.");
+            while (Math.round(c.getAktuellerTankFuellstand()) > 0 && !c.isEngineDefect()) {
+                int drivenKM = c.drive(55);
+                if (drivenKM != 55) {
+
+                    if (c.isEngineDefect()) {
+                        System.out.println("Ja! Motor defekt");
+                    } else {
+                        System.out.println("Dein Tank ist leer; GEHE tanken! Du bist " + drivenKM + " km gefahren.");
+                        float aktuelleMenge = c.Refuel(60);
+                        System.out.printf("Du konntest:%.3f l tanken. ", aktuelleMenge);
+                    }
+
                 }
                 System.out.println("Gefahren Km: " + drivenKM + " Tankinhalt: " + c.getAktuellerTankFuellstand());
             }
+
         }
 
+
+        Engine e = new Engine("Krasser Motor", 500);
+        Engine oldEngine = mygarage.firstElement().changeEngine(e);
+        System.out.println(oldEngine.getName());
     }
 
 }
+
