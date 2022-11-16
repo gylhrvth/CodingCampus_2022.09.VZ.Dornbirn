17828c877c789491722d986bc6351533850e7352
Fanz <franzwinkler@hotmail.com>
2022-11-16 14:19:50 +0100

    Bug Fix

--- src/main/java/franz/week08/Cars/Garage.java
+++ src/main/java/franz/week08/Cars/Garage.java
@@ -10,7 +10,7 @@ public class Garage {
 
         mygarage.add(new Car("Mazda", "3", 45, "Diesel", 1250, new Engine("v8", 100
         )));
-        mygarage.add(new SelfRepairingCar("VW", "Touran", 55, "Diesel", 1520, new Engine("w12", 77)));
+//        mygarage.add(new SelfRepairingCar("VW", "Touran", 55, "Diesel", 1520, new Engine("w12", 77)));
 
         mygarage.add(new AeroDynamicCar("Dacia", "Logan", 50, "Benzin", 1250, new Engine("v6", 200)));
         mygarage.add(new CrapCar("Mercedes", "X350d", 75, "Elektro", 1700, new Engine("E- Motor", 250)));
@@ -25,8 +25,8 @@ public class Garage {
                         System.out.println("Ja! Motor defekt");
                     } else {
                         System.out.println("Dein Tank ist leer; GEHE tanken! Du bist " + drivenKM + " km gefahren.");
-                        float aktuelleMenge = c.Refuel(60);
-                        System.out.printf("Du konntest:%.3f l tanken. ", aktuelleMenge);
+//                        float aktuelleMenge = c.Refuel(60);
+//                        System.out.printf("Du konntest:%.3f l tanken. ", aktuelleMenge);
                     }
 
                 }
