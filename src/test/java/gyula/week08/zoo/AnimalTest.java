5ff198181706c5df0c8d8bcc391e88e6b65eb110
Gyula Horvath <gylhrvth@gmail.com>
2022-11-16 10:20:15 +0100

    JUnit Test Example

---
+++ src/test/java/gyula/week08/zoo/AnimalTest.java
@@ -0,0 +1,16 @@
+package gyula.week08.zoo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+
+    @Test
+    void printStructure() {
+    }
+
+    @Test
+    void getName() {
+    }
+}
