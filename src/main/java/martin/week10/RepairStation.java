0fa0ba72b75ecf527a13c2871d0a8121fd59305d
Martin <Martin.Widhalm@gmx.at>
2022-11-16 10:35:31 +0100

    Update Car-simulation

--- src/main/java/martin/week10/RepairStation.java
+++ src/main/java/martin/week10/RepairStation.java
@@ -1,4 +1,40 @@
 package martin.week10;
 
+import martin.week09.EngineType;
+import martin.week10.Engine;
+
+import java.util.Random;
+
 public class RepairStation {
+    private String name;
+    public static final Random rand = new Random();
+    private Engine[] avEngines;
+
+    public RepairStation(String name, int ammAvEngines) {
+        this.name = name;
+        this.avEngines = new Engine[ammAvEngines];
+        fillRepairStation();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public Engine makeRandEngine() {
+        EngineType[] enTypes = EngineType.values();
+        int enTy = rand.nextInt(EngineType.values().length);
+        return new Engine(
+                enTypes[enTy],
+                100 + rand.nextInt(700, 900),
+                50 + rand.nextInt(0, 50)
+        );
+    }
+
+    public void fillRepairStation() {
+        for (int i = 0; i < avEngines.length; i++) {
+            avEngines[i] = makeRandEngine();
+        }
+    }
 }
