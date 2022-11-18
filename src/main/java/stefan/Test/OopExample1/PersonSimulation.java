c20ca7482f9a2caad4427ef3d9eaa019e79bbac8
stefan0092 <stefan.sardelic@gmail.com>
2022-11-18 11:48:27 +0100

    18.11.2022

--- src/main/java/stefan/Test/OopExample1/PersonSimulation.java
+++ src/main/java/stefan/Test/OopExample1/PersonSimulation.java
@@ -7,41 +7,20 @@ public class PersonSimulation {
 
     public static void main(String[] args) {
         List<Person> personlist2 = new ArrayList<>();
-        Person person1 = new Person("Sahra", 30);
-        Person person2 = new Person("Tom", 38);
-        Person person3 = new Person("Anna", 23);
-        Person person4 = new Person("Martin", 18);
-        Person person5 = new Person("Alex", 9);
-        Person person6 = new Person("john", 67);
+        Person person1 = new Person("Sahra",12, Person.Geslecht.DIVERSE);
+        Person person2 = new Person("Tom",33 , Person.Geslecht.DIVERSE);
+        Person person3 = new Person("Anna",44 , Person.Geslecht.FEMALE);
+        Person person4 = new Person("Martin",15, Person.Geslecht.FEMALE);
+        Person person5 = new Person("Alex", 55,Person.Geslecht.MALE);
+        Person person6 = new Person("john",75, Person.Geslecht.MALE);
         personlist2.add(person1);
         personlist2.add(person2);
         personlist2.add(person3);
         personlist2.add(person4);
         personlist2.add(person5);
         personlist2.add(person6);
-        System.out.println("________________");
-        allsleep(personlist2);
-        System.out.println("________________");
-        allefemale(personlist2);
-        System.out.println("_________________");
-        allemale(personlist2);
+        System.out.println(personlist2);
     }
 
-    public static void allsleep(List<Person> personList) {
-        for (Person p : personList) {
-            p.schlafen();
 
-        }
-    }
-
-    public static void allefemale(List<Person> personLis) {
-        for (Person f : personLis) {
-            f.female();
-        }
-    }
-public static void allemale(List<Person>personList){
-    for (Person p:personList) {
-        p.male();
-    }
-}
 }
