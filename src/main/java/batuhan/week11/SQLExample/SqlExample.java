c4d4b3dc9104456a4380d7497e30751d435b61a4
batuhan <batuhandanisment@hotmail.com>
2022-12-01 12:20:28 +0100

    sql

--- src/main/java/batuhan/week11/SQLExample/SqlExample.java
+++ src/main/java/batuhan/week11/SQLExample/SqlExample.java
@@ -1,49 +1,48 @@
 package batuhan.week11.SQLExample;
 
-import com.mysql.cj.jdbc.PreparedStatementWrapper;
-import com.sun.jdi.connect.spi.Connection;
+//import java.sql.*;
 
-import java.sql.*;
 import java.util.Scanner;
 
 public class SqlExample {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Geben Sie ein Land Name ein");
-        String land = sc.nextLine();
-
-        try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.178.66:3306/mondial?user=root&password=Alperen5858");
-            System.out.println("Verbindung zum SQL Server wurde erfolgreich aufgebaut. :-)");
-
-            PreparedStatement ps = conn.prepareStatement("SELECT * \n" +
-                    "FROM city\n" +
-                    "JOIN country ON city.country = country.code\n" +
-                    "WHERE country.name = ?");
-            ps.setString(1, land);
-            ResultSet rs = ps.executeQuery();
-            ResultSetMetaData metaData = rs.getMetaData();
-
-            for (int i = 1; i <= metaData.getColumnCount(); i++) {
-                System.out.printf("| %" +
-                        (metaData.getColumnTypeName(i).equals("VARCHAR")?"-":"")
-                        + metaData.getPrecision(i) + "s ", metaData.getColumnName(i));
-            }
-            System.out.println("|");
-            while (rs.next()){
-                for (int i = 1; i <= metaData.getColumnCount() ; i++) {
-                    System.out.printf("| %" +
-                            (metaData.getColumnTypeName(i).equals("VARCHAR")?"-":"")
-                            + metaData.getPrecision(i) + "s ", rs.getString(i));
-                }
-                System.out.println("|");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Geben Sie ein Land Name ein");
+//        String land = sc.nextLine();
+//
+//        try {
+//            Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.178.66:3306/mondial?user=root&password=Alperen5858");
+//            System.out.println("Verbindung zum SQL Server wurde erfolgreich aufgebaut. :-)");
+//
+//            PreparedStatement ps = conn.prepareStatement("SELECT * \n" +
+//                    "FROM city\n" +
+//                    "JOIN country ON city.country = country.code\n" +
+//                    "WHERE country.name = ?");
+//            ps.setString(1, land);
+//            ResultSet rs = ps.executeQuery();
+//            ResultSetMetaData metaData = rs.getMetaData();
+//
+//            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+//                System.out.printf("| %" +
+//                        (metaData.getColumnTypeName(i).equals("VARCHAR")?"-":"")
+//                        + metaData.getPrecision(i) + "s ", metaData.getColumnName(i));
+//            }
+//            System.out.println("|");
+//            while (rs.next()){
+//                for (int i = 1; i <= metaData.getColumnCount() ; i++) {
+//                    System.out.printf("| %" +
+//                            (metaData.getColumnTypeName(i).equals("VARCHAR")?"-":"")
+//                            + metaData.getPrecision(i) + "s ", rs.getString(i));
+//                }
+//                System.out.println("|");
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
     }
-
 }
