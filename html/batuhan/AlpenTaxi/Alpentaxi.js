2903ed7bc86eacf06142f642034e79016cc6fad9
batuhan <batuhandanisment@hotmail.com>
2022-12-19 10:11:36 +0100

    Clicker

--- html/batuhan/AlpenTaxi/Alpentaxi.js
+++ html/batuhan/AlpenTaxi/Alpentaxi.js
@@ -3,8 +3,8 @@ setInterval(() => {
     let container = document.getElementById('bigimgContainer')
 
     //console.log("Hello", counter % container.children.length)
-    for (let i = 0; i < container.children.length; ++i){
-        if (i == counter % container.children.length){
+    for (let i = 0; i < container.children.length; ++i) {
+        if (i == counter % container.children.length) {
             container.children[i].classList.add('imageVisible')
         } else {
             container.children[i].classList.remove('imageVisible')
